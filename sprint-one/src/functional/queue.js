337a8436e0d57215aeb13655c4de06177be53b00
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-05 12:23:46 -0800

    queue almost complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var i = 0;
   someInstance.enqueue = function(value) {
+    storage[i] = value;
+    i++;
+
   };
 
   someInstance.dequeue = function() {
+    var deletedItem = 'item';
+    for (key in storage) {
+      deletedItem = storage[key];
+      delete storage[0];
+    }
+    return deletedItem;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (var key in storage) {
+      counter ++;
+    }
+    return counter;
   };
 
   return someInstance;
