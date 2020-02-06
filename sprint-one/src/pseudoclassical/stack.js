01d427a4fc94fd9b2f35e0c94073a97adb4eb21f
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-06 13:26:58 -0800

    completed pseudoclassical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.someInstance = {};
+  this.storage = {};
+  this.i = 0;
+  this.recentlyAddedIndex;
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.i] = value;
+  this.recentlyAddedIndex = this.i;
+  this.i ++;
+};
+
+Stack.prototype.pop = function() {
+  var toDelete = this.storage[this.recentlyAddedIndex];
+  delete this.storage[this.recentlyAddedIndex];
+  this.recentlyAddedIndex --;
+  return toDelete;
+
+};
+
+Stack.prototype.size = function() {
+  var size = 0;
+  for (key in this.storage) {
+    size++;
+  }
+  return size;
+};
+
 
 
