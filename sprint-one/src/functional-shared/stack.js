3cec31991712f0fcb570463e9ff316de4e82b4fa
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-06 11:04:31 -0800

    complete stack prototype

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,43 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+  _.extend(someInstance, stackMethods);
+  return someInstance;
+};
+var i = 0;
+var recentlyAddedIndex;
+var stackMethods = {};
+
+stackMethods.push = function(value) {
+  // have a counter out of function to use as the key
+  // assign key = index, and value = the argument
+  this.storage[i] = value;
+  // increase the counter
+  recentlyAddedIndex = i;
+  i ++;
 };
 
-var stackMethods = {};
+stackMethods.pop = function() {
+  // we have to track the most recently added item
+
+  // keep track of item to be deleted
+  var toDelete = this.storage[recentlyAddedIndex];
+  // delete the property that has recently been added
+  delete this.storage[recentlyAddedIndex];
+  recentlyAddedIndex --;
+  return toDelete;
+
+};
+
+stackMethods.size = function() {
+  var size = 0;
+  for (key in this.storage) {
+    size++;
+  }
+  return size;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  storage: {},
+  i: 0,
+  recentlyAddedIndex: null
+};
 
+stackMethods.push = function(value) {
+  this.storage[this.i] = value;
+  this.recentlyAddedIndex = this.i;
+  this.i ++;
+};
 
+stackMethods.pop = function() {
+  var recentlyDeleted = this.storage[this.recentlyAddedIndex];
+  delete this.storage[this.recentlyAddedIndex];
+  this.recentlyAddedIndex--;
+  return recentlyDeleted;
+};
+
+stackMethods.size = function() {
+  var size = 0;
+  for ( key in this.storage) {
+    size++;
+  }
+  return size;
+};
