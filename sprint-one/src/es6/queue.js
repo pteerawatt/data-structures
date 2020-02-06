b36e6d8fe5f35b4d86751e35eaa60f5c27c3366a
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-02-06 15:36:19 -0800

    completed part1

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,36 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.someInstance = {
+      storage: {},
+      i: 0
+    };
+  }
+
+  enqueue(value) {
+    this.someInstance.storage[this.someInstance.i] = value;
+    this.someInstance.i ++;
+  }
+
+  dequeue() {
+    var deletedItem = 'item';
+    var deleted = false;
+    for (var key in this.someInstance.storage) {
+      if (!deleted) {
+        deletedItem = this.someInstance.storage[key];
+        delete this.someInstance.storage[key];
+        deleted = true;
+      }
+    }
+    return deletedItem;
+  }
+
+  size() {
+    var size = 0;
+    for (var key in this.someInstance.storage) {
+      size++;
+    }
+    return size;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,31 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.someInstance = {
+      storage: {},
+      i: 0,
+      recentlyAddedIndex: null
+    };
   }
 
+  push(value) {
+    this.someInstance.storage[this.someInstance.i] = value;
+    this.someInstance.recentlyAddedIndex = this.someInstance.i;
+    this.someInstance.i ++;
+  }
+
+  pop() {
+    var toDelete = this.someInstance.storage[this.someInstance.recentlyAddedIndex];
+    delete this.someInstance.storage[this.someInstance.recentlyAddedIndex];
+    this.someInstance.recentlyAddedIndex --;
+    return toDelete;
+  }
+
+  size() {
+    var size = 0;
+    for (var key in this.someInstance.storage) {
+      size++;
+    }
+    return size;
+  }
 }
